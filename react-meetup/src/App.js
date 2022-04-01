6f1f2ea38aa6cc3f0c0e290cb43c57870a558d7d
Miyoung Lim <miyoung.lim.dev@gmail.com>
2022-04-02 08:39:53 +0900

    add Main Navigation Bar and update README.md

--- react-meetup/src/App.js
+++ react-meetup/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
@@ -6,6 +7,7 @@ import NewMeetupPage from "./pages/NewMeetup";
 function App() {
   return (
     <div>
+      <MainNavigation/>
       {/**
        * 2022년 현재 Router v6를 사용하므로
        * 다음과 같이 <Routes/> 컴포넌트로 감싸서 사용하도록 변경하였다.
--- react-meetup/src/index.js
+++ react-meetup/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import * as ReactDOMClient from 'react-dom/client';
+import ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 /**
  * React v18 이후로는 ReactDomClient.createRoot를 이용하여
  * root 컴포넌트를 사용하여 render 하도록 권장한다.
  * render 함수 마지막에 context를 입력하던 파라미터가 사라졌다.
  */
-const container = document.getElementById("root");
-const root      = ReactDOMClient.createRoot(container);
-root.render(<BrowserRouter><App /></BrowserRouter>);
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
