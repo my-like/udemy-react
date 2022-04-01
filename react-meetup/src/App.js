1ff778d38e575355d5fa445a3d80bbddf3217112
Miyoung Lim <miyoung.lim.dev@gmail.com>
2022-04-02 00:04:38 +0900

    add comment ( React v18, router v6 )

--- react-meetup/src/App.js
+++ react-meetup/src/App.js
@@ -6,6 +6,10 @@ import NewMeetupPage from "./pages/NewMeetup";
 function App() {
   return (
     <div>
+      {/**
+       * 2022년 현재 Router v6를 사용하므로
+       * 다음과 같이 <Routes/> 컴포넌트로 감싸서 사용하도록 변경하였다.
+       */}
       <Routes>
         <Route path="/" element={<AllMeetupsPage/>} exact={true}/>
         <Route path="/new-meetup" element={<NewMeetupPage/>}/>
--- react-meetup/src/index.js
+++ react-meetup/src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 
+/**
+ * React v18 이후로는 ReactDomClient.createRoot를 이용하여
+ * root 컴포넌트를 사용하여 render 하도록 권장한다.
+ * render 함수 마지막에 context를 입력하던 파라미터가 사라졌다.
+ */
 const container = document.getElementById("root");
 const root      = ReactDOMClient.createRoot(container);
 root.render(<BrowserRouter><App /></BrowserRouter>);
